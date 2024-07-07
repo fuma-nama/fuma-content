6837ea9aee73598e3ec27a6e336dd63168cda199
fuma-nama <god63820869@gmail.com>
2024-07-07 17:29:12 +0800

    Fix SVG id

--- apps/web/components/logo.tsx
+++ apps/web/components/logo.tsx
@@ -1,15 +1,17 @@
-import type { SVGAttributes } from "react";
+import { type SVGAttributes, useId } from "react";
 
 export function Logo(props: SVGAttributes<SVGSVGElement>): JSX.Element {
+  const id = useId();
+
   return (
     <svg width="360" height="360" viewBox="0 0 360 360" {...props}>
       <path
         d="M340 180C340 268.366 268.366 340 180 340C91.6344 340 20 268.366 20 180C20 91.6344 91.6344 20 180 20C180 221 340 91.6344 340 180Z"
-        fill="url(#paint0_radial_89_18)"
+        fill={`url(#${id}-gradient)`}
       />
       <defs>
         <radialGradient
-          id="paint0_radial_89_18"
+          id={`${id}-gradient`}
           cx="0"
           cy="0"
           r="1"
