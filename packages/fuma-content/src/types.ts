da717838bc574ca015b4cbb194f9e01261473d3f
Fuma Nama <god63820869@gmail.com>
2026-03-13 15:25:30 +0800

    fix type

--- packages/fuma-content/src/types.ts
+++ packages/fuma-content/src/types.ts
@@ -1,13 +1,19 @@
 import type { CollectionsPack, GlobalConfig } from "@/config";
 import type { Collection } from "./collections";
 
+type OrFallback<T, Fallback> = [T] extends [never] ? Fallback : T;
+
 // `Config` should be an object of all exports in config file.
-export type GetCollectionConfig<Config, Name extends string> = Config extends {
-  default: GlobalConfig<infer Collections>;
-}
-  ? Resolve<Collections, Name>
-  : Config extends CollectionsPack
-    ? Resolve<Config, Name>
+export type GetCollectionConfig<Config, Name extends string> =
+  Omit<Config, "default"> extends CollectionsPack
+    ? OrFallback<
+        Resolve<Omit<Config, "default">, Name>,
+        Config extends {
+          default: GlobalConfig<infer Collections>;
+        }
+          ? Resolve<Collections, Name>
+          : never
+      >
     : never;
 
 type Resolve<Map extends CollectionsPack, Name extends string> = Map[Name] extends Collection
